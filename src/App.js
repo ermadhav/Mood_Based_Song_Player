6e0f6d5f8f72a832695f63602b2c9a4761ee301c
ermadhav <madhavtiwari2024@gmail.com>
2025-02-27 11:37:04 +0530

    Added Music player to the web site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 import React from 'react';
 import MusicRecommender from './MusicRecommender';
-
+import MusicPlayer from './MusicPlayer';
 function App() {
     return (
         <div>
             <MusicRecommender />
+            <MusicPlayer />
         </div>
     );
 }
