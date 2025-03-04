9d4cd2fc0606907873d54ed8dc54804c127576b8
ermadhav <madhavtiwari2024@gmail.com>
2025-03-04 12:11:45 +0530

    updates some thing

---
+++ backend/config/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    // WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+};
